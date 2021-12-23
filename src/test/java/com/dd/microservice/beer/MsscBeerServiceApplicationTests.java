24be542beb6ef554ad0b9934df1bdff69737cc3d
DIPANJAN DAS <das.dipanjan@hotmail.com>
2021-12-23 17:52:31 +0530

    Initial Project Created. Closes #1

---
+++ src/test/java/com/dd/microservice/beer/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dd.microservice.beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
